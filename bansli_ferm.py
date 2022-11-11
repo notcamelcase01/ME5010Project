1f13278b5962b51f17e4ec1647e7af4ac299630d
Aman Gautam <13annon@gmail.com>
2022-11-12 02:42:31 +0530

    Fern

--- bansli_ferm.py
+++ bansli_ferm.py
@@ -1,11 +1,16 @@
 import lprandom as lprng
 import matplotlib.pyplot as plt
 import numpy as np
+import pseudo_random_gen as pnr
 plt.style.use('dark_background')
 
-n = 10000
-fig, axx = plt.subplots(1, 1, figsize=(9, 9))
-sampl, seed = lprng.lprandom_real_un(10000)
+n = 100000
+fig, (ax1,ax2) = plt.subplots(1, 2, figsize=(16, 9))
+#sampl, seed = lprng.lprandom_real(n)
+sampl = pnr.get_pnr(n)
+xrng = np.random.uniform(low=0., high=1, size=(n,))
+
+
 
 def plot(points,ax):
     """
@@ -16,34 +21,43 @@ def plot(points,ax):
     xx = [x for (x, y) in points]
     yy = [y for (x, y) in points]
 
-    ax.plot(xx, yy, 'g.',markersize=.1)
+    ax.plot(xx, yy, 'g.',markersize=1)
 
-def getnint(n,n0):
-    return int(n*1000)%n0
+
+
 
 def sierpinski(n,xar,ax):
 
-
-    vertices = [(0.0, 0.0), (0.5, 1.0), (1.0, 0.0)]
     points = []
 
     # initial vertex
-    x, y = vertices[2]
+    x,y = 0,0
 
     for i in range(n):
 
-        # select new vertex
-        vx, vy = vertices[get123(xar[i])]
+        r = xar[i]
+        if r < 0.01:
+            x, y = 0.00 * x + 0.00 * y, 0.00 * x + 0.16 * y + 0.00
+        elif r < 0.86:
+            x, y = 0.85 * x + 0.04 * y, -0.04 * x + 0.85 * y + 1.60
+        elif r < 0.93:
+            x, y = 0.20 * x - 0.26 * y, 0.23 * x + 0.22 * y + 1.60
+        else:
+            x, y = -0.15 * x + 0.28 * y, 0.26 * x + 0.24 * y + 0.44
 
 
-        # get middle point
-        x = (vx + x) / 2.0
-        y = (vy + y) / 2.0
-
-
-        points.append((x, y))
+        points.append((x,y))
 
 
     plot(points,ax)
 
 
+sierpinski(n,sampl,ax2)
+sierpinski(n,xrng,ax1)
+
+ax1.set_title("NumpyRand")
+ax2.set_title("Logistic")
+
+
+
+plt.show()
